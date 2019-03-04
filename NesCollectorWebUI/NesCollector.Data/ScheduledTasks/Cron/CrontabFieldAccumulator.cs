9081ffe7ff31d78e2e1190fd7bc8d04b820c949b
james <james@jungledisk.com>
2019-03-04 10:32:55 -0600

    started importing scheduler stuff

---
+++ NesCollectorWebUI/NesCollector.Data/ScheduledTasks/Cron/CrontabFieldAccumulator.cs
@@ -0,0 +1,4 @@
+﻿namespace NesCollector.Data.ScheduledTasks.Cron
+{
+    public delegate void CrontabFieldAccumulator(int start, int end, int interval);
+}
---
+++ NesCollectorWebUI/NesCollector.Data/ScheduledTasks/PriceUpdaterTask.cs
@@ -0,0 +1,19 @@
+﻿using NesCollector.Data.ScheduledTasks.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NesCollector.Data.ScheduledTasks
+{
+    class PriceUpdaterTask : IScheduledTask
+    {
+        public string Schedule => "* */6 * * *";
+
+        public Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
